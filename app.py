a99e1d01f45c954b78883661bd005d3a0b20c785
Vineet Kumar Gupta <vk779756@gmail.com>
2020-10-01 12:55:29 +0530

    Update app.py
    added option for deployment and development now , template can be easily deployed on development servers such as heroku

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template
+evn = 0
+#if evn ==0 then deployment server or else if  development server 
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -6,4 +9,8 @@ def home_page():
     return "Hello World"
 
 if __name__ == '__main__':
-   app.run(debug = True)
+    if (env == 0):
+      port = int(os.environ.get('PORT', 5000))
+      app.run(host='0.0.0.0', port=port)
+    else :
+        app.run(debug = True)
